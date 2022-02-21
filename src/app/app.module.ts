414125a6061f0a8ddafd3a269fbd5c9948ec9fca
Vimal Patel <vimal.patel.nvs@gmail.com>
2022-02-21 17:48:49 +0530

    feat: product list feature added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { metaReducers, reducers } from './reducers';
 import { CounterComponent } from './counter/counter.component';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductModule } from './features/product/product.module';
 
 @NgModule({
     declarations: [AppComponent, CounterComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
+        ProductModule,
         CoreModule,
         StoreModule.forRoot(reducers, {
             metaReducers
--- src/app/features/product/components/product-list/product-list.component.ts
+++ src/app/features/product/components/product-list/product-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
-    constructor() {}
+export class ProductListComponent {
+    products$: Observable<Product[]> = this.productService.getProducts();
 
-    ngOnInit(): void {}
+    constructor(private productService: ProductService) {}
 }
