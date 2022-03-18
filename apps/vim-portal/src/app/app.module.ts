7210a99cc78ec1951c643804cbd8ce073160b5e0
Vimal Patel <vimal.patel.nvs@gmail.com>
2022-03-18 22:21:33 +0530

    feat: ngrx packages added

--- apps/vim-portal/src/app/app.module.ts
+++ apps/vim-portal/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule, 
+    StoreModule.forRoot({}, {}), 
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), 
+    EffectsModule.forRoot([]), 
+    StoreRouterConnectingModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
