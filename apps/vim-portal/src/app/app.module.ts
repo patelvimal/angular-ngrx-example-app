9ff45b588c2a77dcb38fc91e4334c633ad8f8159
Vimal Patel <vimal.patel.nvs@gmail.com>
2022-03-19 20:03:23 +0530

    feat: angular material integration

--- apps/vim-portal/src/app/app.module.ts
+++ apps/vim-portal/src/app/app.module.ts
@@ -8,16 +8,19 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [
-    BrowserModule, 
-    StoreModule.forRoot({}, {}), 
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), 
-    EffectsModule.forRoot([]), 
-    StoreRouterConnectingModule.forRoot()],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NxWelcomeComponent],
+    imports: [
+        BrowserModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule.forRoot(),
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
